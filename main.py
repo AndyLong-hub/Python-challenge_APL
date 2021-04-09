cbeb23cb80e7cce3bf7167a676e83bfa33024e84
AndyLong-hub <andphilon@gmail.com>
2021-04-08 20:37:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -71,53 +71,78 @@ def PyBank():
 
 
 def PyPoll():
-##PyPoll
-##
-##
-##
-##In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
-##
-##
-##You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate.
-##Your task is to create a Python script that analyzes the votes and calculates each of the following:
-##
-##
-##The total number of votes cast
-##
-##
-##A complete list of candidates who received votes
-##
-##
-##The percentage of votes each candidate won
-##
-##
-##The total number of votes each candidate won
-##
-##
-##The winner of the election based on popular vote.
-##
-##
-##
-##
-##As an example, your analysis should look similar to the one below:
-##Election Results
-##-------------------------
-##Total Votes: 3521001
-##-------------------------
-##Khan: 63.000% (2218231)
-##Correy: 20.000% (704200)
-##Li: 14.000% (492940)
-##O'Tooley: 3.000% (105630)
-##-------------------------
-##Winner: Khan
-##-------------------------
-##
-##
-##In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+    # nested list of csv data
+    election_data = []
+
+
+    # open election_data.csv
+    with open(currDir + '/Resources/election_data.csv', 'r') as csv_data:
+        
+
+        # warn user of delay (lots of data to load)
+        print("Loading data. This will take a few moments. . . ")
+
+        
+        # iterate through data and store in list
+        for line in csv.reader(csv_data):
+            election_data.append(line)
+
+
+    # remove header from list
+    election_data.pop(0)
+
+
+    # total number of votes cast
+    total_votes = len(election_data)
+
+    
+    # complete list of candidates who received votes
+    candidates = set()
+    print("Identifying candidates. . . ")
+    for row in election_data:
+        candidates.add(row[2])
+
+
+    # store candidate analysis in dictionary
+    c_analysis = {}
+    for c in candidates:
+
+        # store analysis output in list for each candidate key
+        c_analysis[c] = []
+
+
+    # percentage of votes each candidate won
+    for row in election_data:
+        
+
+    ##The total number of votes each candidate won
+    ##
+    ##
+    ##The winner of the election based on popular vote.
+    ##
+    ##
+    ##
+    ##
+    ##As an example, your analysis should look similar to the one below:
+    ##Election Results
+    ##-------------------------
+    ##Total Votes: 3521001
+    ##-------------------------
+    ##Khan: 63.000% (2218231)
+    ##Correy: 20.000% (704200)
+    ##Li: 14.000% (492940)
+    ##O'Tooley: 3.000% (105630)
+    ##-------------------------
+    ##Winner: Khan
+    ##-------------------------
+    ##
+    ##
+    ##In addition, your final script should both print the analysis to the terminal and export a text file with the results.
 
 
     
-    return None
+    return election_data
 
 
 
@@ -151,6 +176,7 @@ input("Press any button to run PyPoll. . . \n")
 
 
 # run PyPoll script
+election_results = PyPoll()
 
 
 

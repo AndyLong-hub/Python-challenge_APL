8743f06ee99449d60b26429f81f9f6614f2a8023
AndyLong-hub <andphilon@gmail.com>
2021-04-08 20:17:09 -0700

    started PyPoll

--- main.py
+++ main.py
@@ -13,10 +13,9 @@ def PyBank():
 
     # open budget_data.csv
     with open(currDir + '/Resources/budget_data.csv', 'r') as csv_data:
-        csv_reader = csv.reader(csv_data)
 
         # iterate through data and store in list
-        for line in csv_reader:
+        for line in csv.reader(csv_data):
             budget_data.append(line)
 
 
@@ -71,6 +70,57 @@ def PyBank():
     return output
 
 
+def PyPoll():
+##PyPoll
+##
+##
+##
+##In this challenge, you are tasked with helping a small, rural town modernize its vote counting process.
+##
+##
+##You will be give a set of poll data called election_data.csv. The dataset is composed of three columns: Voter ID, County, and Candidate.
+##Your task is to create a Python script that analyzes the votes and calculates each of the following:
+##
+##
+##The total number of votes cast
+##
+##
+##A complete list of candidates who received votes
+##
+##
+##The percentage of votes each candidate won
+##
+##
+##The total number of votes each candidate won
+##
+##
+##The winner of the election based on popular vote.
+##
+##
+##
+##
+##As an example, your analysis should look similar to the one below:
+##Election Results
+##-------------------------
+##Total Votes: 3521001
+##-------------------------
+##Khan: 63.000% (2218231)
+##Correy: 20.000% (704200)
+##Li: 14.000% (492940)
+##O'Tooley: 3.000% (105630)
+##-------------------------
+##Winner: Khan
+##-------------------------
+##
+##
+##In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+
+
+    
+    return None
+
+
+
 def outputFile(file_name, output):
 
     # export to .txt file in Analysis folder
@@ -84,6 +134,7 @@ def outputFile(file_name, output):
     return None
 
 
+
 # current working directory
 currDir = os.getcwd()
 
@@ -94,9 +145,10 @@ input("Press any button to run PyBank. . . ")
 finance_analysis = PyBank()
 print(f"Running PyBank script. . .\n\n{finance_analysis}")
 outputFile("PyBank", finance_analysis)
-print("PyBank completed successfully!")
+print("PyBank completed successfully!\n")
 
-input("Press any button to run PyPoll. . . ")
+input("Press any button to run PyPoll. . . \n")
+
 
 # run PyPoll script
 
